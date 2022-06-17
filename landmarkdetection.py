07e5cce3308be96f19247a8c1f511fcf6abacc94
itzThillaiC <79466435+itzThillaiC@users.noreply.github.com>
2022-06-18 00:27:37 +0530

    File updated

--- landmarkdetection.py
+++ landmarkdetection.py
@@ -10,7 +10,7 @@ hog_face_detector = dlib.get_frontal_face_detector()
 dlib_facelandmark = dlib.shape_predictor(
     "shape_predictor_68_face_landmarks.dat")
 while True:
-    , .frame = cap.read()
+    , frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = hog_face_detector(gray)
@@ -20,3 +20,11 @@ while True:
         for n in range(0, 68):
             x = face_landmarks.part(n).x
             y = face_landmarks.part(n).y
+
+    cv2.imshow("Face Landmarks", frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+cap.release()
+cv2.destroyAllWindows()
