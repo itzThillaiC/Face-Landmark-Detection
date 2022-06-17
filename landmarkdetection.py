685a2e47356820821714f7851ea725d393a446d3
itzThillaiC <79466435+itzThillaiC@users.noreply.github.com>
2022-06-18 00:25:25 +0530

    File updated

--- landmarkdetection.py
+++ landmarkdetection.py
@@ -1,6 +1,22 @@
+from tkinter import Frame
+from tkinter.font import names
 import cv2
+from nbformat import read
 import dlib
 
 cap = cv2.VideoCapture(0)
 
 hog_face_detector = dlib.get_frontal_face_detector()
+dlib_facelandmark = dlib.shape_predictor(
+    "shape_predictor_68_face_landmarks.dat")
+while True:
+    , .frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = hog_face_detector(gray)
+    for face in faces:
+        face_landmarks = dlib_facelandmark(gray, face)
+
+        for n in range(0, 68):
+            x = face_landmarks.part(n).x
+            y = face_landmarks.part(n).y
